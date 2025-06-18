1a294233fe484f0ac33088189cb3075783d9f03b
ErikSalgado2000 <erik.salgado@tecnico-honduras.com>
2025-06-17 22:06:31 -0600

    Add files via upload

---
+++ Java/javascript.js
@@ -0,0 +1,73 @@
+window.onload = function () {
+  // código para dar vuelta a las páginas
+  const pageTurnBtn = document.querySelectorAll('.nextprev-btn');
+
+  pageTurnBtn.forEach((el, index) => {
+    el.onclick = () => {
+      const pageTurnId = el.getAttribute('data-page');
+      const pageTurn = document.getElementById(pageTurnId);
+
+      if (pageTurn.classList.contains('turn')) {
+        pageTurn.classList.remove('turn');
+        setTimeout(() => {
+          pageTurn.style.zIndex = 20 - index;
+        }, 100);
+      } else {
+        pageTurn.classList.add('turn');
+        setTimeout(() => {
+          pageTurn.style.zIndex = index + 20;
+        }, 100);
+      }
+    };
+  });
+
+  const pages = document.querySelectorAll('.book-page.page-right');
+  const contactMeBtn = document.querySelector('.btn.contact-me');
+
+  if (contactMeBtn) {
+    contactMeBtn.onclick = () => {
+      pages.forEach((page, index) => {
+        setTimeout(() => {
+          page.classList.add('turn');
+
+          setTimeout(() => {
+            page.style.zIndex = 20 + index;
+          }, 100);
+        }, (index + 1) * 200 + 100);
+      });
+    };
+  }
+
+  // animación de abrir portafolio
+  const coverRight = document.querySelector('.cover.cover-right');
+
+  if (coverRight) {
+    setTimeout(() => {
+      coverRight.classList.add('turn');
+    }, 2100);
+
+    setTimeout(() => {
+      coverRight.style.zIndex = -1;
+    }, 3300);
+  }
+
+  let totalPages = pages.length;
+  let pageNumber = totalPages - 1;
+
+  function reverseIndex() {
+    pageNumber = ((pageNumber - 1) + totalPages) % totalPages;
+  }
+
+  // animación de apertura de todas las páginas (efecto libro)
+  pages.forEach((_, index) => {
+    setTimeout(() => {
+      pages[pageNumber].classList.remove('turn');
+
+      setTimeout(() => {
+        pages[pageNumber].style.zIndex = index + 10;
+        reverseIndex();
+      }, 100);
+
+    }, (index + 1) * 200 + 3000); // CORREGIDO este tiempo
+  });
+};
